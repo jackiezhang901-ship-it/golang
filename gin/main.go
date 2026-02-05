97362aafdeccc7925e53fe5cf1ef817e136ab619
jackie <wenjiezhang13242945698@gmail.com>
2026-02-05 22:19:00 +0800

    init

--- gin/main.go
+++ gin/main.go
@@ -1,45 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"web/controller"
-	"web/db"
-	. "web/log"
-	. "web/middleware"
-
-	_ "net/http/pprof"
-
-	"github.com/gin-gonic/gin"
+	"web/cmd"
 )
 
 func main() {
 
-	go func() {
-		http.ListenAndServe("localhost:6060", nil)
-	}()
-
-	fmt.Printf("start to run service")
-	Logger.Info("start to launch application")
-	Logger.Info("start to initiate db")
-	db.InitDB()
-	r := gin.Default()
-	r.Use(Cors())
-	api := r.Group("api/user")
-	{
-		api.GET("", controller.SelectUserById)
-		api.POST("", controller.InserUser)
-		api.PUT(":id", controller.UpdateUserInfo)
-		api.GET("/list", controller.SelectUserList)
-	}
-
-	r.Group("api/order")
-	{
-		r.GET("", controller.GetOrders)
-		r.POST("", controller.AddOrder)
-		r.PUT(":id", controller.UpdateOrderInfo)
-	}
-	r.Run(":8080")
-	Logger.Info("application launch successuflly")
+	cmd.Execute()
 
 }
